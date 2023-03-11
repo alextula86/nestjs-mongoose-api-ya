eef3af50602f38427a503397804d56cabbb2ef3d
alextula26 <alex_marc86@mail.ru>
2023-03-11 16:10:28 +0300

    Добавил пакеты passport

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-// import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { useContainer } from 'class-validator';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 import { ErrorExceptionFilter, HttpExceptionFilter } from './exception.filter';
 
 const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy');
   app.enableCors();
-  // app.use(cookieParser());
+  app.use(cookieParser());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
